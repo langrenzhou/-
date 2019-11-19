4c935fe5159735125c288a1791ab74eabc867a85
langrenhou <3393700582@qq.com>
2019-11-19 15:03:12 +0800

    注册接口提交

--- shuju/app.js
+++ shuju/app.js
@@ -23,6 +23,20 @@ app.all("/mysql",function(req,res){
       }
     })
 })
+app.all("/register",function(req,res){
+    res.header("Access-Control-Allow-Origin", "*"); //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Headers", "content-type"); //允许的header类型
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    var sql=`insert into t_users  set username="a",password="123" ,nikiname="yuzhong",email="3555@qq";`
+    lianjie.query(sql,function(err,result){
+      if(err){
+          res.send(err)
+      }else{
+          res.send(result)
+          res.end(err)
+      }
+    })
+})
 app.listen("3030",function(){
     console.log("端口号是127.0.0.1:3030")
 })
